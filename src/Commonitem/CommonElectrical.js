1bf451cfc6282ecdb77fdfb3c695739f97830ad4
bhandarinikesh <nikeshbhandari00@gmail.com>
2023-12-13 21:33:41 +0545

    main

--- src/Commonitem/CommonElectrical.js
+++ src/Commonitem/CommonElectrical.js
@@ -1,7 +1,34 @@
 
 
-import electricGoods1 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
-import electricGoods2 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods1 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods2 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods3 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods4 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods5 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods6 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods7 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods8 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods9 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods10 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods11 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods12 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods13 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods14 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods15 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods16 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods17 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods18 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods19 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods20 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods21 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods22 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods23 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods24 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods25 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+import elecGoods26 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods27 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
+import elecGoods28 from 'assets/images/Elecrtical/ElectricGoods/Heater.jpg';
+// import elecGoods26 from 'assets/images/Elecrtical/ElectricGoods/holder.jpg';
 
 import imagechangover from 'assets/images/Elecrtical/House Wiring/changover.jpg';
 import imageThreePhase from 'assets/images/Elecrtical/House Wiring/ThreePhase.jpg';
@@ -15,8 +42,9 @@ import waterPumps from 'assets/images/Elecrtical/Good Repair/waterPumps.jpg';
 import weldingMechine from 'assets/images/Elecrtical/Good Repair/weldingMechine.jpg';
 
 
+
 export const  CommonElectricGoods=[ 
-    {icon:electricGoods1,
+    {icon:elecGoods1,
      id:'1',
      price:'1000 to 4800',
      name:'All wire',
@@ -25,7 +53,228 @@ export const  CommonElectricGoods=[
      discount:'20'
     },
     
-    {icon:electricGoods2,
+    {icon:elecGoods2,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods3,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods4,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods5,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods6,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods7,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods8,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods9,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods10,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods11,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods12,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods13,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods14,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods15,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods16,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods17,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods18,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods19,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods20,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods21,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods22,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods23,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods24,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods25,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods26,
+     id:'2',
+     price:'120 to 5000',
+     name:'MCB to SF6CB',
+     rate:'Change able ',
+     address:'lalitpur',
+     discount:'20'
+    },
+    {icon:elecGoods27,
+     id:'1',
+     price:'1000 to 4800',
+     name:'All wire',
+     rate:'12 to 32',
+     address:'lalitpur ',
+     discount:'20'
+    },
+    
+    {icon:elecGoods28,
      id:'2',
      price:'120 to 5000',
      name:'MCB to SF6CB',
@@ -34,6 +283,7 @@ export const  CommonElectricGoods=[
      discount:'20'
     },
 ]
+
 
 export const  CommonHouseWiring=[ 
     {icon:imagechangover,
